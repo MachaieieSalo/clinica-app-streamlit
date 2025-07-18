571e57d650f881828b612b03847c6cc8bc7d272c
Salomão Machaieie <135821028+MachaieieSalo@users.noreply.github.com>
2025-07-18 15:41:17 +0200

    app.py

--- app.py
+++ app.py
@@ -356,6 +356,7 @@ def gerar_pdf_cotacao_fpdf(empresa, itens):  # Renomeei para evitar conflito com
     )
 
     # Dados da empresa
+    elementos.append(Spacer(1, 12))
     elementos.append(Paragraph("Boane, B.2, Q.3 Av. Namaacha, Rua 1º de Maio C. Nº 59",  estilo_normal))
     elementos.append(Paragraph("NUIT: 401937684",  estilo_normal))
     elementos.append(Paragraph("Contacto: +258  84 671 1512 / 87 791 1717",  estilo_normal))
